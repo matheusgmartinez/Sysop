3116635b97ece150d492685f51674336ac9498d7
matheus gonzalez <mathe555@outlook.com>
2023-06-07 19:54:41 -0300

    projeto sysop v1

---
+++ src/main/java/com/sysop/objects/Usuario.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sysop.objects;
+
+/**
+ *
+ * @author CAA201
+ */
+public class Usuario {
+
+    private String nome;
+    private String senha;
+    private Integer id;
+    private String cpf;
+
+    public Usuario(String nome, String senha, Integer id, String cpf) {
+        this.nome = nome;
+        this.senha = senha;
+        this.id = id;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
+
